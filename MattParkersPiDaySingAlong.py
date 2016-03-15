dd52421a034259a04c3a16e41c3b7ecb90dbe3ff
TheEJM3 <edwardmclean913@gmail.com>
2016-03-14 21:19:13 -0400

    Finalized
    Commented, added inputs for max and update rate

--- MattParkersPiDaySingAlong.py
+++ MattParkersPiDaySingAlong.py
@@ -1,19 +1,52 @@
+#Matt Parker's Pi Day Sing Along
+
+#Edward J. McLean III
+#Pi Day 2016!
+#Raymond, NH
+
+#Matt Parker made a YouTube vid about calculating Pi by hand
+#using an infinite sum that tends towards pi.  The sum is:
+#1/1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11... infinity.
+#He did the first 10 fractions by hand ending on ...-1/19 and
+#the sum was 3.04.  I wanted to know exactly how many fractions
+#it would take to get a decimal accurate to 314 digits of pi.
+
+#Using the script below, I figured out that the difference 
+#between the sum and pi is slightly less than 1/# of fractions. For 
+#example, the sum of the first 1,000,000 fractions in the series
+#differs from pi by < 1/1,000,000 or .000001.
+
 import math
 from decimal import *
-getcontext().prec = 315
-pi = Decimal(3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660632)
+#precision will be good for first billion iterations
+getcontext().prec = 10
+#number of decimal points match precision
+pi = Decimal(3.1415926536)
+#variable used as running total of factors
 runsum = Decimal(0.0)
+#starts on 2 because -1^x will return + on each odd fraction
 x = 2
+#variable used for periodic, printed updates
 y = 1
-while Decimal(runsum*4) - pi != Decimal(0.00):
-	while y < 1000001:
-		runsum = runsum + (((-1)**x)*(Decimal(1.000)/(x+(x-3))))
-		x = x + 1
-		y = y + 1
-	print "x =", x-2
-	print "Difference =", Decimal(runsum*4) - pi
-	y = 1
+#iterations will stop at this amount
+max = input("Enter Max Iterations: ")
+#screen output at each time this amount is reached
+z = input("Enter Update Rate: ")
+#main loop:
+for n in xrange((x-2), max):
+	#returns next fraction then sums the previous fractions with current
+	runsum = runsum + (((-1)**x)*(Decimal(1.000)/(x+(x-3))))
+	#updates current count
+	y = y + 1
+	#updates current x value
+	x = x + 1
+	#if current count matches update rate, print update
+	if y == z + 1:
+		print "x =", x-2
+		print "Current sum of factors =", runsum*4
+		print "Difference between sum and pi=", Decimal(runsum*4) - pi
+		y = 1
+#when completed, print final values
 print "x =", x-2
-print "Difference =", Decimal(runsum*4) - pi
-print "runsum*4 =", runsum*4
-print "It will not be 314 digits precise until you add 1/", (2*(x-2)-1)
+print "Current sum of factors =", runsum*4
+print "Difference between sum and pi=", Decimal(runsum*4) - pi
